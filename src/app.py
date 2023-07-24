fbabb3e12e1ee72ecf57408d0b088720eb09fd27
LuckMeelo <charmeel.vodouhe@epitech.eu>
2023-07-24 17:48:27 +0100

    feat: logger, on_raw_reaction_add

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 import sys, os
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
+import random
 import json
 import platform
 import asyncio
+
+import logger
 
 EXTENSIONS = [
     "cogs.roles",
@@ -24,32 +27,109 @@ def load_config(filepath: str):
 
 config = load_config("config.json")
 intents = discord.Intents.default()
+intents.members = True  # Subscribe to the privileged members intent
+intents.reactions = True  # Enable reaction events
 intents.message_content = True
 
 class App(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=config["prefix"],
                         intents=intents) # missing a help command
+        self.logger = logger.create(name="discord_bot", logfilename="discord.log")
 
     async def on_ready(self):
         '''
         Print general infos about the bot and the platform its running on.
         '''
-        print(f"Logged in as {self.user.name}")
-        print(f"discord.py API version: {discord.__version__}")
-        print(f"Python version: {platform.python_version()}")
-        print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
-        print("-------------------")
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+        self.logger.info("-------------------")
+        self.status_task.start() # start status loop task
+
 
     async def on_message(self, message: discord.Message) -> None:
         '''
         Process every messages sent by users and trigger the appropriate
         command.
         '''
-        print("message")
         if message.author == self.user or message.author.bot:
             return
         await self.process_commands(message)
+
+    async def on_command_completion(self, context: commands.Context) -> None:
+        """
+        The code in this event is executed every time a normal command has been *successfully* executed.
+
+        :param context: The context of the command that has been executed.
+        """
+        full_command_name = context.command.qualified_name
+        split = full_command_name.split(" ")
+        executed_command = str(split[0])
+        if context.guild is not None:
+            self.logger.info(
+                f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
+            )
+        else:
+            self.logger.info(
+                f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs"
+            )
+
+    async def on_command_error(self, context: commands.Context, error) -> None:
+        """
+        The code in this event is executed every time a normal valid command catches an error.
+
+        :param context: The context of the normal command that failed executing.
+        :param error: The error that has been faced.
+        """
+        if isinstance(error, commands.CommandNotFound):
+            self.logger.warning(
+                f"Command not found: {context.message.content} by {context.author} (ID: {context.author.id})"
+            )
+        elif isinstance(error, commands.CommandOnCooldown):
+            minutes, seconds = divmod(error.retry_after, 60)
+            hours, minutes = divmod(minutes, 60)
+            hours = hours % 24
+            embed = discord.Embed(
+                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                description="You are missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to execute this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(
+                description="I am missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to fully perform this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title="Error!",
+                # We need to capitalize because the command arguments have no capital letter in the code.
+                description=str(error).capitalize(),
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        else:
+            raise error
+
+    @tasks.loop(seconds=20)
+    async def status_task(self) -> None:
+        """
+        Setup the game status task of the bot.
+        """
+        statuses = ["avec toi!", "avec Krypton!", "avec les humains!"]
+        await self.change_presence(activity=discord.Game(random.choice(statuses)))
 
     def run(self):
         super().run(config['token'], reconnect=True)
--- src/cogs/roles.py
+++ src/cogs/roles.py
@@ -1,4 +1,6 @@
+import discord
 from discord.ext import commands
+
 
 class Roles(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -6,13 +8,37 @@ class Roles(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self) -> None:
-        print("roles ready")
-        pass
+        self.bot.logger.info(f"Roles Cog ready")
 
     @commands.command()
     async def roles(self, ctx: commands.Context) -> None:
-        print("roles")
-        pass
+        self.bot.logger.info(f"roles command executed")
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload : discord.RawReactionActionEvent):
+        guild_id = payload.guild_id
+        channel_id = payload.channel_id
+        message_id = payload.message_id
+        user_id = payload.user_id
+        emoji = payload.emoji
+        self.bot.logger.info(f"reaction added")        
+
+        # Check if the reaction is from the bot
+        if user_id == self.bot.user.id:
+            return
+
+        # Check if the reaction is added to the correct message
+        if message_id == YOUR_MESSAGE_ID:  # Replace YOUR_MESSAGE_ID with the actual message ID
+            guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
+            if guild:
+                role_name = "YOUR_ROLE_NAME"  # Replace YOUR_ROLE_NAME with the desired role name
+                role = discord.utils.get(guild.roles, name=role_name)
+                if role:
+                    member = guild.get_member(user_id)
+                    if member:
+                        await member.add_roles(role)
+                        self.bot.logger.info(f"Added role {role.name} to {member.name}")        
+
 
 async def setup(bot):
     await bot.add_cog(Roles(bot))
